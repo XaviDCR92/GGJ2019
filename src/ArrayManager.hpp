61ac7a2145cd93c9c4e589213c14892ed15f04dc
Iker <ikereizagirre@hotmail.com>
2019-01-26 09:47:57 +0100

    ArrayManager to be started

---
+++ src/ArrayManager.hpp
@@ -0,0 +1,16 @@
+template<class T>
+class ArrayManager
+{
+public:
+
+    ArrayManager(unsigned size)
+    {
+        
+    }
+    //T* AddElement(const T& element );
+
+private:
+
+    unsigned mSize;
+    T* mArray;
+};
